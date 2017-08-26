f39d4cd7eb257f7d04668157730d695d8e87f728
Mehran Rahbardar <mrahbardar@users.noreply.github.com>
2017-08-26 11:25:36 +0430

    Create config.js

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+config = {
+    sso:'__http://SERVICE.ENDPOINT/_',
+    sso_service:'__http://SERVICE.ENDPOINT/oauth__',
+    client_id:'__CLIENT_ID__',
+    client_secret:'__CLIENT_SECRET__',
+    home:'http://example.com'
+};  
