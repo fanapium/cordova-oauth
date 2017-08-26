2930f90c85ee2bd9145dd2cb560170444a2b8bde
Mehran Rahbardar <mrahbardar@users.noreply.github.com>
2017-08-26 11:24:07 +0430

    Delete config.js

--- www/js/config.js
+++
@@ -1,7 +0,0 @@
-config = {
-    sso:'http://sandbox.fanapium.com/oauth2/',
-    sso_service:'http://sandbox.fanapium.com/',
-    client_id:'9327444a9d4b266a7d6c1c76',
-    client_secret:'7a034aac',
-    home:'http://79.174.163.69/'
-};
